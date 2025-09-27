0e8f1f682e010dd46d10bc8d2d988daf117a3395
novaprospectCAU <novaprospect110a@gmail.com>
2025-09-27 17:29:19 +0900

    fix: wip

--- src/includes/glad/khrplatform.h
+++ src/includes/glad/khrplatform.h
@@ -140,7 +140,8 @@
 /*-------------------------------------------------------------------------
  * basic type definitions
  *-----------------------------------------------------------------------*/
-#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) ||              \
+    defined(__GNUC__) || defined(__SCO__) || defined(__USLC__)
 
 /*
  * Using <stdint.h>
@@ -300,8 +301,7 @@ typedef khronos_int64_t khronos_stime_nanoseconds_t;
  * Values other than zero should be considered to be true.  Therefore
  * comparisons should not be made against KHRONOS_TRUE.
  */
-typedef enum
-{
+typedef enum {
   KHRONOS_FALSE = 0,
   KHRONOS_TRUE = 1,
   KHRONOS_BOOLEAN_ENUM_FORCE_SIZE = KHRONOS_MAX_ENUM
